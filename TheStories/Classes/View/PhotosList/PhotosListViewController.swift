1cc6d7e2be078a41538af60e39fbc78541666ec1
William <will.liiam06@gmail.com>
2019-06-28 16:54:22 +0700

    feat: add photos list viper

---
+++ TheStories/Classes/View/PhotosList/PhotosListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotosListViewController.swift
+//  TheStories
+//
+//  Created by Wil Liam on 6/28/19.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+import UIKit
+
+class PhotosListViewController: UIViewController, PhotosListView {
+    var event: PhotosListEvent?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
